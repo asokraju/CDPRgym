b965706c6efac5390a4522541d95d462a3c0db70
Krishna Chaitanya Kosaraju <kkrishnachaitanya89@gmail.com>
2022-09-13 13:32:27 -0400

    Bugfix

--- envs/cdpr.py
+++ envs/cdpr.py
@@ -164,18 +164,18 @@ class PRPRmodel():
     j = 1
     l = l0
     tau = tau0
-    l_dot = (l_des - l0)/self.dtdyn;
+    l_dot = (l_des - l0)/Ts;
     l_dot_min = np.array([-0.05,-0.05,-0.05,-0.05]).T
     l_dot_max = np.array([0.05,0.05,0.05,0.05]).T
-    l_dot = np.array([-0.01,2,-0.1,4]).T
+    #l_dot = np.array([-0.01,2,-0.1,4]).T
     l_dot = [max(x, y) for x, y in zip(l_dot, l_dot_min)] 
     l_dot = [min(x, y) for x, y in zip(l_dot, l_dot_max)]
-    tau_dot = (tau_des - tau0)/self.dtdyn;
-    tau_dot_min = np.array([-0.05,-0.05,-0.05,-0.05]).T
-    tau_dot_max = np.array([0.05,0.05,0.05,0.05]).T
-    tau_dot = np.array([-0.01,2,-0.1,4]).T
-    tau_dot = [max(x, y) for x, y in zip(tau_dot, tau_dot_min)] 
-    tau_dot = [min(x, y) for x, y in zip(tau_dot, tau_dot_max)]
+    tau_dot = (tau_des - tau0)/Ts;
+    #tau_dot_min = np.array([-0.05,-0.05,-0.05,-0.05]).T
+    #tau_dot_max = np.array([0.05,0.05,0.05,0.05]).T
+    #tau_dot = np.array([-0.01,2,-0.1,4]).T
+    #tau_dot = [max(x, y) for x, y in zip(tau_dot, tau_dot_min)] 
+    #tau_dot = [min(x, y) for x, y in zip(tau_dot, tau_dot_max)]
 
     for j in range(1,int(Ts/self.dtdyn)):
       P_x = self.structureMatrix(X_dyn[0:3],l)
@@ -213,7 +213,7 @@ class PRPRmodel():
     if kappa < 0.:
       mom = np.array([0.], dtype=np.float32)
 
-    return min(kappa,mom)
+    return kappa,mom
 
 
 class CDPRenv(PRPRmodel,gym.Env):
@@ -232,7 +232,7 @@ class CDPRenv(PRPRmodel,gym.Env):
          np.finfo(np.float32).max, #y_e^t
          np.finfo(np.float32).max, #\dot x_e^t
          np.finfo(np.float32).max, #\dot y_e^t
-         2., # kappa+mon
+         2., # kappa+mom
          1., # tensions
          1., # tensions
          1., # tensions
@@ -314,7 +314,7 @@ class CDPRenv(PRPRmodel,gym.Env):
     R2 = 0.
     R3 = np.linalg.norm(self.velocity_error) 
     R4 = np.matmul((np.array([0.1,0.1,0.1,0.1]) - self.T0).T, (np.array([0.1,0.1,0.1,0.1]) - self.T0))
-    R5 = (1 - self.kappa[0])
+    R5 = (1 - min(self.kappa[0],self.mom[0]))
 
     weights = np.array([1e4,0.,0.,1e3,1e1,1e1]).reshape(1,6)
     reward = np.array([R0,R1,R2,R3,R4,R5]).reshape(1,6)
@@ -327,12 +327,12 @@ class CDPRenv(PRPRmodel,gym.Env):
     T_des,l_des = self.action_scaling(action)
     X_dyn0 = self.state[:6]
 
-    if self.steps == 1:
+    if self.steps == 0:
       self.T0 = T_des
       self.l0 = l_des
   
     X_dyn,l1,T1 = self.fwd_dyn(X_dyn0,l_des.T,self.l0,T_des.T,self.T0,self.Ts)
-    self.kappa = self.poseQuality(X_dyn[0:3],l1)
+    self.kappa,self.mom = self.poseQuality(X_dyn[0:3],l1)
 
     self.T0 = T1
     self.l0 = l1
@@ -346,7 +346,7 @@ class CDPRenv(PRPRmodel,gym.Env):
     self.position_error = X_dyn[0:2] - self.X_des[0:2,self.steps]
     self.velocity_error = X_dyn[3:5] - self.X_des[3:5,self.steps]
 
-    self.state = np.concatenate((X_dyn,self.position_error,self.velocity_error,self.kappa,T1,l1))
+    self.state = np.concatenate((X_dyn,self.position_error,self.velocity_error,self.mom+self.kappa,T1,l1))
 
     
 
@@ -354,6 +354,5 @@ class CDPRenv(PRPRmodel,gym.Env):
       self.done = True
 
     self.steps = self.steps + 1
-    return self.state, self.reward(), self.done, {}
-
+    return self.state, self.reward()/1e5, self.done, {}
 
