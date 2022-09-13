567b36836abb3f5a0554b14fefbb2c91bbe47c8e
Krishna Chaitanya Kosaraju <kkrishnachaitanya89@gmail.com>
2022-09-13 14:12:41 -0400

    Bugfix

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from gym.envs.registration import register
 
 logger = logging.getLogger(__name__)
 
-register(id="CDPR-v0", entry_point="from CDPRGYM.envs.cdpr import CDPRenv")
+register(id="CDPR-v0", entry_point="CDPRgym.envs.cdpr:CDPRenv")
 
 # gym.envs.register(
 #  id='BicycleKin-v0',
